573ec6c54eb9afad72b872420df7c7711bd13719
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-01-28 02:07:59 +0100

    Add navigate back button to product site

--- src/components/ProductOverview.js
+++ src/components/ProductOverview.js
@@ -1,11 +1,20 @@
-import './styles/ProductOverview.css'
+import './styles/ProductOverview.css';
+import LeftArrow from './img/misc/left-arrow.svg';
 import { Link } from 'react-router-dom';
 
+function NavigateBackToShop() {
+    return (
+        <Link to="/shop" className="navigate-back">
+            <img src={LeftArrow}></img>
+        </Link>  
+    )
+}
+
 function ProductOverview(props) {
-    const id = 'what'
+    const id = 'what';
     return (
         <div className="product-overview">
-
+            <NavigateBackToShop />
         </div>
     )
 }
