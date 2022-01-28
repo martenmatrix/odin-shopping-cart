31005fcb87ddad51d73d3681e221d4e0b52595d7
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-01-28 01:19:39 +0100

    Add ProductOverview js and css file

---
+++ src/components/ProductOverview.js
@@ -0,0 +1,13 @@
+import './styles/ProductOverview.css'
+import { Link } from 'react-router-dom';
+
+function ProductOverview(props) {
+    const id = 'what'
+    return (
+        <div className="product-overview">
+
+        </div>
+    )
+}
+
+export default ProductOverview;
