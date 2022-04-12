2049aeddcce34de84f32a179ce1e45558319d405
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-04-12 12:25:15 +0200

    Fix basename because of renaming

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ function Layout() {
 
 function App() {
   return (
-      <BrowserRouter basename="/odin-shopping-cart">
+      <BrowserRouter basename="/shopping-cart">
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Homepage shopPath='shop'/>} />
